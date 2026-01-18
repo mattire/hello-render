04e8e51533ec8ed149b1e292dc9e324819e7cc3d
mattire <alr2d4@hotmail.com>
2026-01-18 17:36:06 +0200

    Hello World Node web service

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Render provides the PORT env variable
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Render!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
